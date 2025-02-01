fb20f35af5fc9379f2dcdcca51ddce1103003eb2
Prathmesh-Mane-7 <manejay288@gmail.com>
2025-02-01 19:04:40 +0530

    remove add feature

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,7 +1,7 @@
 class Calculator{
-    public int add(int a,int b){
-        return a+b;
-    }
+    // public int add(int a,int b){
+    //     return a+b;
+    // }
 
     public int sub(int a,int b){
         return a-b;
@@ -12,7 +12,7 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("result : " + calc.add(10,20));
+        // System.out.println("result : " + calc.add(10,20));    remove kela so git hub red
 
         System.out.println("result : " + calc.sub(10,20));
 

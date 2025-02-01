58eb807bf923e346fa11a6b889060afbfbd68a3c
Prathmesh-Mane-7 <manejay288@gmail.com>
2025-02-01 19:00:49 +0530

    feature of addition added into calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,6 +2,10 @@ class Calculator{
     public int add(int a,int b){
         return a+b;
     }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
 }
 
 public class CalculatorTest {
@@ -9,5 +13,8 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("result : " + calc.add(10,20));
+
+        System.out.println("result : " + calc.sub(10,20));
+
     }
 }
